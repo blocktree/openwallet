7ec9e2c8b17c9b5ff42a3c2b317be60cc9541eef
陈志文 <279887897@qq.com>
2018-08-16 16:41:09 +0800

    连接rpc成功，代码暂存

---
+++ assets/qtum/config.go
@@ -0,0 +1,201 @@
+/*
+ * Copyright 2018 The OpenWallet Authors
+ * This file is part of the OpenWallet library.
+ *
+ * The OpenWallet library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The OpenWallet library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ */
+
+package qtum
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/astaxie/beego/config"
+	"github.com/blocktree/OpenWallet/common/file"
+	"path/filepath"
+	"strings"
+	"github.com/shopspring/decimal"
+	"time"
+)
+
+/*
+	工具可以读取各个币种钱包的默认的配置资料，
+	币种钱包的配置资料放在conf/{symbol}.conf，例如：ADA.conf, BTC.conf，ETH.conf。
+	执行wmd wallet -s <symbol> 命令会先检查是否存在该币种钱包的配置文件。
+	没有：执行ConfigFlow，配置文件初始化。
+	有：执行常规命令。
+	使用者还可以通过wmd config -s 进行修改配置文件。
+	或执行wmd config flow 重新进行一次配置初始化流程。
+
+*/
+
+const (
+	//币种
+	Symbol    = "QTUM"
+	MasterKey = "qtum seed"
+)
+
+var (
+	//RPC认证账户名
+	rpcUser = "test"
+	//RPC认证账户密码
+	rpcPassword = "test1234"
+	//钥匙备份路径
+	keyDir = filepath.Join("data", strings.ToLower(Symbol), "key")
+	//地址导出路径
+	addressDir = filepath.Join("data", strings.ToLower(Symbol), "address")
+	//区块链数据
+	//blockchainDir = filepath.Join("data", strings.ToLower(Symbol), "blockchain")
+	//配置文件路径
+	configFilePath = filepath.Join("conf")
+	//配置文件名
+	configFileName = Symbol + ".ini"
+	//区块链数据文件
+	blockchainFile = "blockchain.db"
+	//是否测试网络
+	isTestNet = true
+	// 核心钱包是否只做监听
+	CoreWalletWatchOnly = true
+	//最大的输入数量
+	maxTxInputs = 50
+	//本地数据库文件路径
+	dbPath = filepath.Join("data", strings.ToLower(Symbol), "db")
+	//备份路径
+	backupDir = filepath.Join("data", strings.ToLower(Symbol), "backup")
+	//钱包服务API
+	serverAPI = "http://120.78.220.105:3889"
+	//钱包安装的路径
+	nodeInstallPath = ""
+	//钱包数据文件目录
+	walletDataPath = ""
+	//小数位长度
+	coinDecimal decimal.Decimal = decimal.NewFromFloat(100000000)
+	//参与汇总的钱包
+	walletsInSum = make(map[string]*Wallet)
+	//汇总阀值
+	threshold decimal.Decimal = decimal.NewFromFloat(5)
+	//汇总地址
+	sumAddress = ""
+	//汇总执行间隔时间
+	cycleSeconds = time.Second * 10
+	//默认配置内容
+	defaultConfig = `
+# start node command
+startNodeCMD = ""
+# stop node command
+stopNodeCMD = ""
+# node install path
+nodeInstallPath = ""
+# mainnet data path
+mainNetDataPath = ""
+# testnet data path
+testNetDataPath = ""
+# node api url
+apiURL = ""
+# RPC Authentication Username
+rpcUser = ""
+# RPC Authentication Password
+rpcPassword = ""
+# Is network test?
+isTestNet = false
+# the safe address that wallet send money to.
+sumAddress = ""
+# when wallet's balance is over this value, the wallet willl send money to [sumAddress]
+threshold = ""
+`
+)
+
+func setWalletConfig(sumAddress string, threshold string) {
+
+}
+
+//newConfigFile 创建配置文件
+func newConfigFile(
+	apiURL, walletPath, sumAddress string,
+	threshold string, isTestNet bool) (config.Configer, string, error) {
+
+	//	生成配置
+	configMap := map[string]interface{}{
+		"apiURL":        apiURL,
+		"walletPath":    walletPath,
+		"sumAddress":    sumAddress,
+		"threshold":     threshold,
+		"isTestNet":     isTestNet,
+	}
+
+	filepath.Join()
+
+	bytes, err := json.Marshal(configMap)
+	if err != nil {
+		return nil, "", err
+	}
+
+	//实例化配置
+	c, err := config.NewConfigData("json", bytes)
+	if err != nil {
+		return nil, "", err
+	}
+
+	//写入配置到文件
+	file.MkdirAll(configFilePath)
+	absFile := filepath.Join(configFilePath, configFileName)
+	err = c.SaveConfigFile(absFile)
+	if err != nil {
+		return nil, "", err
+	}
+
+	return c, absFile, nil
+}
+
+//printConfig Print config information
+func printConfig() error {
+
+	initConfig()
+	//读取配置
+	absFile := filepath.Join(configFilePath, configFileName)
+	//apiURL := c.String("apiURL")
+	//walletPath := c.String("walletPath")
+	//threshold := c.String("threshold")
+	//minSendAmount := c.String("minSendAmount")
+	//minFees := c.String("minFees")
+	//sumAddress := c.String("sumAddress")
+	//isTestNet, _ := c.Bool("isTestNet")
+
+	fmt.Printf("-----------------------------------------------------------\n")
+	//fmt.Printf("Wallet API URL: %s\n", apiURL)
+	//fmt.Printf("Wallet Data FilePath: %s\n", walletPath)
+	//fmt.Printf("Summary Address: %s\n", sumAddress)
+	//fmt.Printf("Summary Threshold: %s\n", threshold)
+	//fmt.Printf("Min Send Amount: %s\n", minSendAmount)
+	//fmt.Printf("Transfer Fees: %s\n", minFees)
+	//if isTestNet {
+	//	fmt.Printf("Network: TestNet\n")
+	//} else {
+	//	fmt.Printf("Network: MainNet\n")
+	//}
+	file.PrintFile(absFile)
+	fmt.Printf("-----------------------------------------------------------\n")
+
+	return nil
+
+}
+
+//initConfig 初始化配置文件
+func initConfig() {
+
+	//读取配置
+	absFile := filepath.Join(configFilePath, configFileName)
+	if !file.Exists(absFile) {
+		file.MkdirAll(configFilePath)
+		file.WriteFile(absFile, []byte(defaultConfig), false)
+	}
+
+}
