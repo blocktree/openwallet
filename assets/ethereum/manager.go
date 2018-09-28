2d2437dada47016a0a1da9832cbc8e5915a57b49
孙扬 <peter@sunyangdeMacBook-Pro.local>
2018-09-28 20:19:30 +0800

    modify log

--- assets/ethereum/manager.go
+++ assets/ethereum/manager.go
@@ -1321,7 +1321,7 @@ func (this *WalletManager) ERC20SummaryWallets() {
 }
 
 func (this *WalletManager) SummaryWallets() {
-	log.Debugf("[Summary Wallet Start]------%s\n", common.TimeFormat("2006-01-02 15:04:05"))
+	log.Error(fmt.Sprintf("[Summary Wallet Start]------%v\n", common.TimeFormat("2006-01-02 15:04:05")))
 	//读取参与汇总的钱包
 	for _, wallet := range this.WalletInSumOld {
 		balance, err := this.GetWalletBalance(this.GetConfig().DbPath, wallet)
@@ -1331,20 +1331,20 @@ func (this *WalletManager) SummaryWallets() {
 		}
 
 		if balance.Cmp(this.GetConfig().Threshold) > 0 {
-			log.Debugf("Summary account[%s]balance = %v \n", wallet.WalletID, balance)
-			log.Debugf("Summary account[%s]Start Send Transaction\n", wallet.WalletID)
+			log.Error(fmt.Sprintf("Summary account[%v]balance = %v \n", wallet.WalletID, balance))
+			log.Error(fmt.Sprintf("Summary account[%v]Start Send Transaction\n", wallet.WalletID))
 
 			txId, err := this.SendTransaction2(wallet, this.GetConfig().SumAddress, balance, wallet.Password, true)
 			if err != nil {
-				log.Debugf("Summary account[%s]unexpected error: %v\n", wallet.WalletID, err)
+				log.Error(fmt.Sprintf("Summary account[%v]unexpected error: %v\n", wallet.WalletID, err))
 				continue
 			} else {
-				log.Debugf("Summary account[%s]successfully，Received Address[%s], TXID：%s\n", wallet.WalletID, this.GetConfig().SumAddress, txId)
+				log.Error(fmt.Sprintf("Summary account[%v]successfully，Received Address[%v], TXID：%v\n", wallet.WalletID, this.GetConfig().SumAddress, txId)
 			}
 		}
 	}
 
-	log.Debugf("[Summary Wallet end]------%s\n", common.TimeFormat("2006-01-02 15:04:05"))
+	log.Error(fmt.Sprintf("[Summary Wallet end]------%v\n", common.TimeFormat("2006-01-02 15:04:05")))
 }
 
 func (this *WalletManager) RestoreWallet2(backupPath string, password string) error {
