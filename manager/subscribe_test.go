9acff84e0671c65ba33c88b142143b53eaba2afb
麦志泉 <zhiquan911@qq.com>
2018-10-19 19:03:14 +0800

    manager包新增了单个区块扫描器测试用例

--- manager/subscribe_test.go
+++ manager/subscribe_test.go
@@ -54,3 +54,58 @@ func TestSubscribe(t *testing.T) {
 	<-endRunning
 }
 
+
+////////////////////////// 测试单个扫描器 //////////////////////////
+
+type subscriberSingle struct {
+}
+
+//BlockScanNotify 新区块扫描完成通知
+func (sub *subscriberSingle) BlockScanNotify(header *openwallet.BlockHeader) error {
+	log.Notice("header:", header)
+	return nil
+}
+
+//BlockTxExtractDataNotify 区块提取结果通知
+func (sub *subscriberSingle) BlockExtractDataNotify(sourceKey string, data *openwallet.TxExtractData) error {
+	log.Notice("account:", sourceKey)
+	log.Std.Notice("data.TxInputs: %+v", data.TxInputs)
+	log.Std.Notice("data.TxOutputs: %+v", data.TxOutputs)
+	log.Std.Notice("data.Transaction: %+v", data.Transaction)
+	return nil
+}
+
+func TestSubscribeAddress(t *testing.T) {
+
+	var (
+		endRunning = make(chan bool, 1)
+		symbol = "ETH"
+		accountID = "W4VUMN3wxQcwVEwsRvoyuhrJ95zhyc4zRW"
+		address = "0x558ef7a2b56611ef352b2ecf5d2dd2bf548afecc"
+	)
+
+	assetsMgr, err := GetAssetsManager(symbol)
+	if err != nil {
+		log.Error(symbol, "is not support")
+		return
+	}
+	//log.Debug("already got scanner:", assetsMgr)
+	scanner := assetsMgr.GetBlockScanner()
+	//scanner.SetRescanBlockHeight(4840986)
+
+
+	if scanner == nil {
+		log.Error(symbol, "is not support block scan")
+		return
+	}
+
+	scanner.AddAddress(address, accountID)
+
+	sub := subscriberSingle{}
+	scanner.AddObserver(&sub)
+	//tm.SetRescanBlockHeight("QTUM", 236098)
+
+	scanner.ScanBlock(4840986)
+
+	<-endRunning
+}
