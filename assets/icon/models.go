aaa5ac138417657b758199067ca7a9ee1d1fa122
jx <125089163@qq.com>
2018-10-30 16:41:56 +0800

    add icx

---
+++ assets/icon/models.go
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2018 The OpenWallet Authors
+ * This file is part of the OpenWallet library.
+ *
+ * The OpenWallet library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The OpenWallet library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ */
+
+package icon
+
+import (
+	"os"
+	"strings"
+)
+
+type Key struct {
+	Address    string `storm:"id"`
+	PublicKey  []byte
+	PrivateKey []byte
+}
+
+type Transaction struct {
+	From      string
+	To        string
+	Value     string //0x + Hex string
+	Timestamp string
+	Nonce     string
+	StepLimit string
+	Tx_hash   string
+}
+
+// skipKeyFile ignores editor backups, hidden files and folders/symlinks.
+func skipKeyFile(fi os.FileInfo) bool {
+	// Skip editor backups and UNIX-style hidden files.
+	if strings.HasSuffix(fi.Name(), "~") || strings.HasPrefix(fi.Name(), ".") {
+		return true
+	}
+	// Skip misc special files, directories (yes, symlinks too).
+	if fi.IsDir() || fi.Mode()&os.ModeType != 0 {
+		return true
+	}
+	return false
+}
